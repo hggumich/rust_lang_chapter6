7b2351a8b716aeb20863ff09b0b9b03ae5b62dd6
Hector Garza <hggumich@gmail.com>
2022-10-03 18:00:09 -0400

    finish Option<T>

---
+++ Option_Enum_as_Null/src/main.rs
@@ -0,0 +1,13 @@
+enum Option<T> {
+    None,
+    Some(T),
+}
+
+let some_number =Some(5);
+let some_char = Some('e');
+
+let absent_number: Option<i32> = None;
+
+fn main() {
+    println!("Hello, world!");
+}
